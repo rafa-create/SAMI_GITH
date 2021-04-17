dd4e4328caa9c631fe65b0f1a48c763256b508ac
rafa-create <rafaelorset@hotmail.fr>
2021-04-17 19:42:33 +0200

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -5,10 +5,12 @@ from time import sleep
 
 
 
-class Application():
+class Interface():
     def __init__(self,Points_a_atteindre,position_lego):
         self.Points_a_atteindre=Points_a_atteindre
         self.position_lego=position_lego
+    #initialisation des variables
+        self.s=self.j=self.m=self.t=self.a=self.b=0
     #Création de la fenêtre principale (main window)
         self.Mafenetre=Tk()
         self.Mafenetre.title('Suivi du robot en direct')
@@ -16,7 +18,7 @@ class Application():
         self.can=Canvas(self.Mafenetre,width=1180,height=600,bg='cyan')
         self.can.pack(side=TOP,padx=5,pady=5)
     #informer l'utilisateur sur la couleur du Lego
-        self.lego = self.can.create_rectangle(100,100,110,110,fill='dark green')
+        self.lego = self.can.create_rectangle(100,100,110,110,fill='red')
         self.lu,self.lv,self.chemin = [100,112],[100,112],[]
         self.chemin.append(self.lego)
     #commande au clavier
@@ -39,83 +41,55 @@ class Application():
             pY = randrange(5, 600)
             self.can.create_oval(pX-r, pY-r, pX+r, pY+r,fill="yellow")
     def deplacement(self):
-        global a,b,z,y,j,m
         c=len(self.chemin)
-        sleep(0.3)
         #Chaque carré reprend la coordonnée du précédent dans la liste (chemin)
         while c!=0 :
                 self.lu[c]=self.lu[c-1]
                 self.lv[c]=self.lv[c-1]
                 c+=-1
         self.can.create_rectangle(self.lu[c]+3,self.lv[c]+3,self.lu[c]+7,self.lv[c]+7,fill='black')
-        self.can.coords(self.chemin[c],self.lu[c],self.lv[c],self.lu[c]+10,self.lv[c]+10)
         #On change les coordonées du premier carré
-        self.lu[0] += a
-        self.lv[0] += b
+        self.lu[0] += self.a
+        self.lv[0] += self.b
         #On applique les nouvelles coordonnées aux carrés correspondant
         self.can.coords(self.chemin[c],self.lu[c],self.lv[c],self.lu[c]+10,self.lv[c]+10)
-        c=1
-        #Si le chemin est mord un coté il ressort de l'autre
-        #autre coté du canvevas:
-        d=1
-        if self.lu[0]==200:
-                self.lu[0],d=10,0
-        if self.lu[0]==0 and d==1:
-                self.lu[0]=200
-        if self.lv[0]==200:
-                self.lv[0],d=10,0
-        if self.lv[0]==0 and d==1:
-                self.lv[0]=200
-        d=0
         #Si le lego de tête recoupe le cercle
-        if j!=1 and m!=1:
-                self.Mafenetre.after(100,self.deplacement)
+        if self.j!=1 and self.m!=1:#commande pour la pause
+            self.can.after(100,self.deplacement)
     def gauche(self,event):
-        global a,b,s
-        a=-10
-        b=0
-        if s==0:
-                s=1
+        self.a=-10
+        self.b=0
+        if self.s==0:
+                self.s=1
                 self.deplacement()
     def droite(self,event):
-        global a,b,s
-        a=10
-        b=0
-        if s==0:
-                s=1
+        self.a=10
+        self.b=0
+        if self.s==0:
+                self.s=1
                 self.deplacement()
     def haut(self,event):
-        global a,b,s
-        a=0
-        b=-10
-        if s==0:
-                s=1
+        self.a=0
+        self.b=-10
+        if self.s==0:
+                self.s=1
                 self.deplacement()
     def bas(self,event):
-        global a,b,s
-        a=0
-        b=10
-        if s==0:
-                s=1
+        self.a=0
+        self.b=10
+        if self.s==0:
+                self.s=1
                 self.deplacement()
     def pause(self,event):
-        global j,a,b,m
-        t=0
-        if a==b:
-                t=1
-        if j!=1:
-                #Affichage ou Effacage du texte 'PAUSE'
-                #Et arrêt
-                if m!=1:
-                        m=1
+        self.t=0
+        if self.a==self.b:
+                self.t=1
+        if self.j!=1:
+                if self.m!=1:
+                        self.m=1
                 else:
-                        m=0
-                        if t!=1:
+                        self.m=0
+                        if self.t!=1:
                                 self.deplacement()
 
-s=j=m=t=a=b=0
-z=y=50
-
-
-App=Application(6,7)
-App.Mafenetre.mainloop()
+Interface(8,5).Mafenetre.mainloop()
