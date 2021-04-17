358e2282d8d65641b854b92276e511400547ffbf
RemiS758 <remisalaun758@gmail.com>
2021-04-17 15:21:06 +0200

    Update Squelette.py

--- Squelette.py
+++ Squelette.py
@@ -2,12 +2,21 @@ Go pour le squelette :
 from PlusProcheVoisin import *
 
 class RoboLego():
-    def __init__(x=0,y=0):
-        position=[x,y]
+    def __init__(xinit=0,yinit=0):
+        self.position=[xinit,yinit]
+        self.listeNoeuds=[]
 
     def recupererNoeuds(self,addresse):
         """Prends en parametre l'adresse du fichier texte contenant les noeuds. Renvoie un liste de noeuds."""
+        fichier=open(r'C:\Users\Remis\Documents\GitHub\SAMI\fichier_de_points.txt')
+        txt=fichier.read()
+        points=txt.split("\n")
         listeNoeuds=[]
+        for p in points:
+            coordonnee=p.split("\t")
+            coordonee[0],coordonnee[1]=float(coo[0]),float(coo[1])
+            listeNoeuds.append(coordonnee)
+
         return listeNoeuds
 
     def ordreNoeuds(self,listeNoeuds):
@@ -20,5 +29,6 @@ class RoboLego():
         """Prend en parametre un noeud. Actionne les roues du robot pour le faire se déplacer depuis sa position actuelle vers un noeud objectif."""
 
 
-    def cestPartie(self):
+    def cestPartie(self,adresse):
+        listeNoeuds=ordreNoeuds(recupererAdresse(adresse))
     """Lance le robot. Utilise ordreNoeuds() et allerVers() pour emmener le robot à destination."""
