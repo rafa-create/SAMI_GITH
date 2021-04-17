463f2b70d6f476b103f2d2d89df3ac2875d3cf3b
rafa-create <rafaelorset@hotmail.fr>
2021-04-17 16:59:48 +0200

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -3,9 +3,9 @@ from random import randrange
 
 
 class Application():
-    def __init__(self,Points_a_atteindre,nbr_colonnes):
+    def __init__(self,Points_a_atteindre,poistion_lego):
         self.Points_a_atteindre=Points_a_atteindre
-        self.nbr_colonnes=nbr_colonnes
+        self.poistion_lego=poistion_lego
         self.color = ["red", "#EDEF3A"]
     #Création de la fenêtre principale (main window)
         self.Mafenetre=Tk()
@@ -17,16 +17,17 @@ class Application():
     # zone de dessin
         self.can=Canvas(self.Mafenetre,width=1180,height=600,bg='cyan')
         self.can.pack(side=TOP,padx=5,pady=5)
-    #rectangle avec les info du robot
-        self.can.create_rectangle(18,405,92,425,fill="lightblue")
-        self.can.create_text(47,415,text='Position du Lego',fill="black")
+    #informer l'utilisateur sur la couleur du Lego
+        self.can.create_rectangle(10,405,130,425,fill="lightblue")
+        self.can.create_text(60,415,text='Position du Lego',fill="black")
         r=7
-        self.can.create_oval(77-r,415-r,77+r,415+r,fill="red")
-    #rectangle avec les info des Points_a_atteindre
-        d=100
-        self.can.create_rectangle(18+d,405,d+92,425,fill="lightblue")
-        self.can.create_text(47+d,415,text='Points à atteindre',fill="black")
-        self.can.create_oval(77-r+d,415-r,77+d+r,415+r,fill="yellow")
+        self.can.create_oval(120-r,415-r,120+r,415+r,fill="red")
+    #informer l'utilisateur sur la couleur des Points_a_atteindre
+        d=30
+        self.can.create_rectangle(10,405-d,130,425-d,fill="lightblue")
+        self.can.create_text(60,415-d,text='Points à atteindre',fill="black")
+        self.can.create_oval(120-r,415-d-r,120+r,415+r-d,fill="yellow")
+
         for i in range(7):
             pX = randrange(5, 1180)
             pY = randrange(5, 600)
