1a90570d513accaad922a5fad8f424ac3c00dd97
RemiS758 <remisalaun758@gmail.com>
2021-04-18 17:50:11 +0200

    Update Squelette.py

--- Squelette.py
+++ Squelette.py
@@ -1,16 +1,36 @@
 from PlusProcheVoisin import *
 from Interface import *
+from  ev3dev2.motor  import  OUTPUT_A , OUTPUT_B , LargeMotor
+import sys, os
+import mocap_node as mcn
+
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'src'))
+
+###
 
 class RoboLego():
-    def __init__(self,xinit=0,yinit=0,cst=None):
-        self.__position=[xinit,yinit]
+    def __init__(self,name="Lego1",nomDeConfig="Mon Nom De Config",cst=None):
+        self.__name=name
+
+        self.__mcn=mcn.MocapNode(nomDeConfig)
+
+        self.__mcn.run()
+
+        self.__mcn.UpdateModelInfo()
+        self.__position, self.__yaw = self.__mcn.getPos2DAndYaw(self.__name)
+
+        self.__mcn.stop()
+
         self.__listeNoeuds=[]
         self.__constantes=cst
-        self.__interface=Application(self.__listeNoeuds,self.__position)
+        self.__interface=None
+        self.__lmotor , self.__rmotor = [LargeMotor(address) for  address  in (OUTPUT_A , OUTPUT_B)]
 
-    def recupererNoeuds(self,addresse):
+
+
+    def recupererNoeuds(self,nom_fichier="fichier_de_points"):
         """Prends en parametre l'adresse du fichier texte contenant les noeuds. Renvoie un liste de noeuds."""
-        fichier=open(r'C:\Users\Remis\Documents\GitHub\SAMI\fichier_de_points.txt')
+        fichier=open(nom_fichier)
         txt=fichier.read()
         points=txt.split("\n")
         listeNoeuds=[]
@@ -20,6 +40,7 @@ class RoboLego():
             listeNoeuds.append(coordonnee)
 
         return listeNoeuds
+
 
     def ordreNoeuds(self,listeNoeuds):
         """Prend en parametre une liste de noeuds. Renvoie la liste des noeuds dans le bon ordre de passage. """
@@ -31,12 +52,15 @@ class RoboLego():
         """Prend en parametre une liste de noeuds. Actionne les roues du robot pour le faire se déplacer depuis sa position actuelle vers chaque noeud dans l'ordre de la liste."""
 
 
-    def cestParti(self,adresse):
+
+    def cestParti(self,nom_fichier):
         """Lance le robot. Prend en parametre une adresse Utilise ordreNoeuds() et allerVers() pour emmener le robot à destination."""
-        listeNoeuds=self.ordreNoeuds(self.recupererNoeuds(adresse))
-        self.__interface.Mafenetre.mainloop()
-        self.partieAuto()
-        print("bob")
+        listeNoeuds=self.ordreNoeuds(self.recupererNoeuds(nom_fichier))
+        self.__interface=Application(self.__listeNoeuds,self.__position)
+        self.__mcn.run()
+        # self.__interface.Mafenetre.mainloop()
+        # self.partieAuto()
+        self.__mcn.stop()
 
 ###
 
