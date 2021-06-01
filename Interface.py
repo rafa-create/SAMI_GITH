4541cfd1c01c843b466c9ce720b91ad8839f7a44
rafa-create <rafaelorset@hotmail.fr>
2021-06-01 17:13:28 +0200

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -2,13 +2,12 @@ from random import randrange
 from tkinter import *
 
 class Interface():
-    def __init__(self,Points_a_atteindre,position_lego):
+    def __init__(self,Points_a_atteindre):
     #initialisation des variables graphiques
         self.pres=5
         self.trace=0.5
         self.tlego=14
         self.Points_a_atteindre=Points_a_atteindre
-        self.position_lego=position_lego
         self.cote=600
         self.s=self.j=self.m=self.t=self.a=self.b=0
     #Création de la fenêtre
@@ -17,12 +16,6 @@ class Interface():
     # zone de dessin
         self.can=Canvas(self.Mafenetre,width=self.cote,height=self.cote,bg='cyan')
         self.can.pack(side=TOP,padx=5,pady=5)
-    #commande au clavier
-        self.can.bind_all('<Up>', self.haut)
-        self.can.bind_all('<Down>', self.bas)
-        self.can.bind_all('<Left>', self.gauche)
-        self.can.bind_all('<Right>', self.droite)
-        self.can.bind_all('p',self.pause)
     #informer l'utilisateur sur la couleur du lego
         b=167#décalage vers le bas des panneaux
         rl=7
@@ -37,12 +30,14 @@ class Interface():
         self.can.create_oval(120-r,415-d-r+b,120+r,415+r-d+b,fill="yellow")
     #initialisation lego
         self.lego =self.can.create_oval(120-rl,415-rl+b,120+rl,415+rl+b,fill="red")
-        self.lu,self.lv,self.chemin = [100,112],[100,112],[]
+        self.lu,self.lv,self.chemin = [1000,1000],[10000,1000],[]#hors du cadre
         self.chemin.append(self.lego)
+    #Tracé des points à atteindre
         for i in range(7):
-            pX = randrange(5, self.cote)
-            pY = randrange(5, self.cote)
+            [pX,pY]= self.Points_a_atteindre[i]
             self.can.create_oval(pX-r, pY-r, pX+r, pY+r,fill="yellow")
+    #lancer l'affichage du robot
+        self.deplacement()
     def deplacement(self):
         c=len(self.chemin)
     #Chaque carré reprend la coordonnée du précédent dans la liste (chemin)
@@ -52,47 +47,13 @@ class Interface():
                 c+=-1
     #On fait la trace
         self.can.create_rectangle(self.lu[c]+self.trace+7,self.lv[c]+self.trace+7,self.lu[c]-self.trace+7,self.lv[c]-self.trace+7,fill='black')
-    #On change les coordonées du Lego
-        self.lu[0] += self.a
-        self.lv[0] += self.b
+    #coordonnées du lego ici
+        self.lu[0] =100 #INSERER ABSCISSE ROBOT
+        self.lv[0] =100#INSERER ordonnée ROBOT
     #On applique les nouvelles coordonnées aux carrés correspondant
         self.can.coords(self.chemin[c],self.lu[c],self.lv[c],self.lu[c]+self.tlego,self.lv[c]+self.tlego)
-        if self.j!=1 and self.m!=1:
-            self.can.after(300,self.deplacement)
-    def gauche(self,event):
-        self.a=-self.pres
-        self.b=0
-        if self.s==0:
-                self.s=1
-                self.deplacement()
-    def droite(self,event):
-        self.a=self.pres
-        self.b=0
-        if self.s==0:
-                self.s=1
-                self.deplacement()
-    def haut(self,event):
-        self.a=0
-        self.b=-self.pres
-        if self.s==0:
-                self.s=1
-                self.deplacement()
-    def bas(self,event):
-        self.a=0
-        self.b=self.pres
-        if self.s==0:
-                self.s=1
-                self.deplacement()
-    def pause(self,event):
-        self.t=0
-        if self.a==self.b:
-                self.t=1
-        if self.j!=1:
-                if self.m!=1:
-                        self.m=1
-                else:
-                        self.m=0
-                        if self.t!=1:
-                                self.deplacement()
+    #On peut modifier la vitesse d'actualisation ici
+        self.can.after(300,self.deplacement)
 
-Interface(8,5).Mafenetre.mainloop()
+Points_a_atteindre=[[466,362],[45,444],[54,380],[438,44],[559,129],[243,234],[342,331]]
+Interface(Points_a_atteindre).Mafenetre.mainloop()
